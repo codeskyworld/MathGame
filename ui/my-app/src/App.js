8f9ef2918ffeea8d40a5ae1b716e0d349e93dba0
codeskyworld <codeskyworld@gmail.com>
2022-12-02 15:53:27 +1000

    feat: initial completed

--- ui/my-app/src/App.js
+++ ui/my-app/src/App.js
@@ -1,30 +1,72 @@
 import "./App.css";
 import { Form, FormGroup, Label, Input, Col } from "reactstrap";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { constants } from "./Constants";
+import imgQuestionMark from "./images/mark-question.png";
+import phone1 from "./images/phone1.png";
+import phone2 from "./images/phone2.png";
+import phone3 from "./images/phone3.png";
+import labtop1 from "./images/labtop1.jpg";
+import labtop2 from "./images/labtop2.jpg";
+import labtop3 from "./images/labtop3.jpg";
+import car1 from "./images/car1.png";
+import car2 from "./images/car2.png";
+import car3 from "./images/car3.png";
+import house1 from "./images/house1.jpg";
+import house2 from "./images/house2.jpeg";
+import house3 from "./images/house3.jpg";
+import girlfriend1 from "./images/girlfriend1.jpg";
+import girlfriend2 from "./images/girlfriend2.jpg";
+import girlfriend3 from "./images/girlfriend3.jpg";
 
 function App() {
-  const [number, setNumber] = useState("");
-  const [words, setWords] = useState("");
+  const [instruction, setInstruction] = useState(
+    "Please input your 'Name' and press 'Submit' button to win below gift!"
+  );
+  const [count, setCount] = useState(0);
+  const [arrayTry, setArrayTry] = useState([]);
+  const [inputWord, setInputWord] = useState("");
+  const [randomFirstIndex, setRandomFirstIndex] = useState(0);
+  const [randomSecondIndex, setRandomSecondIndex] = useState(0);
+  const [randomThirdIndex, setRandomThirdIndex] = useState(0);
+  const [randomFourthIndex, setRandomFourthIndex] = useState(0);
+  const [randomFifthIndex, setRandomFifthIndex] = useState(0);
+  const [score, setScore] = useState(0);
+  const [name, setName] = useState(0);
+  const [lockinput, setLockinput] = useState(false);
 
-  const ProcessHandler = async (e) => {
-    e.preventDefault();
-    fetch(constants.API_URL + "number/" + number, {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-    })
-      .then((res) => res.json())
-      .then(
-        (result) => {
-          setWords(result);
-        },
-        (error) => {
-          alert("Failded");
-        }
-      );
+  const [answerArray, setAnswerArray] = useState([]);
+  const [indexFirstDisplay, setIndexFirstDisplay] = useState(false);
+  const [indexSecondDisplay, setIndexSecondDisplay] = useState(false);
+  const [indexThirdDisplay, setIndexThirdDisplay] = useState(false);
+  const [indexFourthDisplay, setIndexFourthDisplay] = useState(false);
+  const [indexFifthDisplay, setIndexFifthDisplay] = useState(false);
+
+  const phone = [phone1, phone2, phone3];
+  const labtop = [labtop1, labtop2, labtop3];
+  const car = [car1, car2, car3];
+  const house = [house1, house2, house3];
+  const girlfriend = [girlfriend1, girlfriend2, girlfriend3];
+
+  useEffect(() => {
+    GetNumberHandler();
+  }, []);
+
+  const AssignValue = (e) => {
+    setArrayTry([
+      `${e[0][0]} + ? = ${e[0][2]}`,
+      `${e[1][0]} - ? = ${e[1][2]}`,
+      `${e[2][0]} x ? = ${e[2][2]}`,
+      `${e[3][0]} ÷ ? = ${e[3][2]}`,
+      `( ${e[4][0]} + ? ) x ( ${e[4][2]} - ${e[4][3]} ）÷ 4 = ${e[4][4]}`,
+    ]);
+    setAnswerArray([
+      `${e[0][1]}`,
+      `${e[1][1]}`,
+      `${e[2][1]}`,
+      `${e[3][1]}`,
+      `${e[4][1]}`,
+    ]);
   };
 
   const GetNumberHandler = async (e) => {
@@ -36,16 +78,119 @@ function App() {
       },
     })
       .then((res) => res.json())
-      .then((response) => console.log(response));
-    // .then((data) => console.log(data));
+      .then((response) => {
+        AssignValue(response);
+      });
   };
-
-  GetNumberHandler();
 
   const ClearHandler = async (e) => {
     e.preventDefault();
-    setNumber("");
-    setWords("");
+    document.getElementById("inputValue").value = "";
+    setInputWord("");
+  };
+
+  const SubmitHandler = async (e) => {
+    e.preventDefault();
+    if (inputWord === "" && count === 0) {
+      alert("Sorry, please input your name!");
+      return;
+    } else if (inputWord === "" && count > 0 && count < 6) {
+      alert("Sorry, please input integer number!");
+      return;
+    }
+    if (count < 5) {
+      setInstruction(arrayTry[count]);
+    }
+    document.getElementById("inputValue").value = "";
+
+    if (count === 0) {
+      setName(inputWord.toString());
+    }
+
+    if (count === 1) {
+      setIndexFirstDisplay(
+        inputWord.toString() === answerArray[count - 1].toString()
+          ? true
+          : false
+      );
+      setRandomFirstIndex(Math.floor(Math.random() * 3));
+      if (inputWord.toString() !== answerArray[0].toString()) {
+        alert(
+          `Sorry, incorrect answer! The correct answer is ${answerArray[0]} and should be integer!`
+        );
+      } else {
+        setScore(score + 20);
+      }
+    } else if (count === 2) {
+      setIndexSecondDisplay(
+        inputWord.toString() === answerArray[count - 1].toString()
+          ? true
+          : false
+      );
+      setRandomSecondIndex(Math.floor(Math.random() * 3));
+      if (inputWord.toString() !== answerArray[1].toString()) {
+        alert(
+          `Sorry, incorrect answer! The correct answer is ${answerArray[1]} and should be integer!`
+        );
+      } else {
+        setScore(score + 20);
+      }
+    } else if (count === 3) {
+      setIndexThirdDisplay(
+        inputWord.toString() === answerArray[count - 1].toString()
+          ? true
+          : false
+      );
+      setRandomThirdIndex(Math.floor(Math.random() * 3));
+      if (inputWord.toString() !== answerArray[2].toString()) {
+        alert(
+          `Sorry, incorrect answer! The correct answer is ${answerArray[2]} and should be integer!`
+        );
+      } else {
+        setScore(score + 20);
+      }
+    } else if (count === 4) {
+      setIndexFourthDisplay(
+        inputWord.toString() === answerArray[count - 1].toString()
+          ? true
+          : false
+      );
+      setRandomFourthIndex(Math.floor(Math.random() * 3));
+      if (inputWord.toString() !== answerArray[3].toString()) {
+        alert(
+          `Sorry, incorrect answer! The correct answer is ${answerArray[3]} and should be integer!`
+        );
+      } else {
+        setScore(score + 20);
+      }
+    } else if (count === 5) {
+      setIndexFifthDisplay(
+        inputWord.toString() === answerArray[count - 1].toString()
+          ? true
+          : false
+      );
+      setRandomFifthIndex(Math.floor(Math.random() * 3));
+      if (inputWord.toString() !== answerArray[4].toString()) {
+        alert(
+          `Sorry, incorrect answer! The correct answer is ${answerArray[4]} and should be integer!`
+        );
+      } else {
+        setScore(score + 20);
+      }
+      setInstruction(
+        `Congratulation! ${name}, You have completed the test. Please click the 'Submit' button to check your grade`
+      );
+      setLockinput(true);
+    } else if (count === 6) {
+      setInstruction(
+        `You final grade is ${score}! Please click the 'Submit' button to collect ${name}'s gift!`
+      );
+    } else if (count >= 7) {
+      window.open(constants.VOLVO_URL, "_blank");
+    }
+
+    setCount(count + 1);
+    setInputWord("");
   };
 
   return (
@@ -56,60 +201,99 @@ function App() {
       <br></br>
       <br></br>
       <br></br>
-      <Form>
+      <Form sm={12}>
         <br></br>
         <br></br>
         <br></br>
         <div className="text-center">
-          <h1 id="loginTitle">Number To Words</h1>
+          <h1 id="loginTitle">Math Game</h1>
+          <h3 id="hintTitle">Hint: Answer is the interger of 1 - 100</h3>
         </div>
-        <FormGroup row>
-          <Label for="number" sm={4}>
-            Number
+
+        <FormGroup row id="inputrow">
+          <Label for="number" sm={12} id="contentInstruction">
+            {instruction}
           </Label>
-          <Col sm={12}>
+          <Col sm={6}>
             <Input
               type="text"
               name="number"
-              id="number"
-              value={number}
+              id="inputValue"
+              disabled={lockinput}
               onChange={(event) => {
-                setNumber(event.target.value);
+                setInputWord([event.target.value]);
               }}
             />
           </Col>
         </FormGroup>
-        <FormGroup row>
-          <Label for="result" sm={5}>
-            Words Result
-          </Label>
-          <Col sm={12}>
-            <div
-              id="resultbar"
-              className="alert alert-primary"
-              role="alert"
-              value={number}
-            >
-              {words}
-            </div>
-          </Col>
-        </FormGroup>
-        <div className="d-grid gap-3 mt-5">
+        <div className="d-grid text-center gap-3 mt-4">
           <button
-            className="btn btn-outline-primary btn-lg w-100"
-            onClick={ProcessHandler}
+            className="btn btn-outline-primary btn-lg w-50"
+            onClick={SubmitHandler}
           >
-            Convert
+            Submit
           </button>
         </div>
-
-        <div className="d-grid gap-3 mt-3">
+        <div className="d-grid text-center gap-3 mt-3">
           <button
-            className="btn btn-outline-danger btn-lg w-100"
+            className="btn btn-outline-danger btn-lg w-50"
             onClick={ClearHandler}
           >
             Clear
           </button>
+        </div>
+
+        <div className="row mt-5">
+          <div className="col">
+            <img
+              src={
+                indexFirstDisplay ? phone[randomFirstIndex] : imgQuestionMark
+              }
+              alt="Instagram"
+              width="190"
+              height="190"
+            />
+          </div>
+          <div className="col">
+            <img
+              src={
+                indexSecondDisplay ? labtop[randomSecondIndex] : imgQuestionMark
+              }
+              alt="Instagram"
+              width="190"
+              height="190"
+            />
+          </div>
+          <div className="col">
+            <img
+              src={indexThirdDisplay ? car[randomThirdIndex] : imgQuestionMark}
+              alt="Instagram"
+              width="190"
+              height="190"
+            />
+          </div>
+          <div className="col">
+            <img
+              src={
+                indexFourthDisplay ? house[randomFourthIndex] : imgQuestionMark
+              }
+              alt="Instagram"
+              width="190"
+              height="190"
+            />
+          </div>
+          <div className="col">
+            <img
+              src={
+                indexFifthDisplay
+                  ? girlfriend[randomFifthIndex]
+                  : imgQuestionMark
+              }
+              alt="Instagram"
+              width="190"
+              height="190"
+            />
+          </div>
         </div>
       </Form>
     </div>
--- ui/my-app/src/Constants.js
+++ ui/my-app/src/Constants.js
@@ -1,3 +1,4 @@
 export const constants = {
   API_URL: "http://localhost:59837/api/",
+  VOLVO_URL: "https://www.volvocars.com/au",
 };
