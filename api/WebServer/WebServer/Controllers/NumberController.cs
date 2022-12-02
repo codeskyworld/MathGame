71588f3e167690f67ac7607cc5f84b94c9db1812
codeskyworld <codeskyworld@gmail.com>
2022-12-02 19:24:18 +1000

    fix: optimize the code

--- api/WebServer/WebServer/Controllers/NumberController.cs
+++ api/WebServer/WebServer/Controllers/NumberController.cs
@@ -15,38 +15,11 @@ namespace WebServer.Controllers
     [ApiController]
     public class NumberController : ControllerBase
     {
-
-        // Used for checking API status
-        private static readonly string[] Summaries = new[]
-                {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-        //[HttpGet]
-        //public IEnumerable<WeatherForecast> Get()
-        //{
-        //    var rng = new Random();
-        //    return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        //    {
-        //        Date = DateTime.Now.AddDays(index),
-        //        TemperatureC = rng.Next(-20, 55),
-        //        Summary = Summaries[rng.Next(Summaries.Length)]
-        //    })
-        //    .ToArray();
-        //}
-
-        [HttpPost("{number}")]
-        public JsonResult Post(string number)
-        {
-                return new JsonResult(ValueConvert.NumberToWords(number));
-        }
-
         [HttpGet]
         public JsonResult Get()
         {
             NumerAndWord numerAndWord = new NumerAndWord();
             return new JsonResult(numerAndWord.GetNumerAndWord());
-            //return new JsonResult("Hello World");
         }
     }
 
--- api/WebServer/WebServer/Program.cs
+++ api/WebServer/WebServer/Program.cs
@@ -1,7 +1,10 @@
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Serialization;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,5 +25,56 @@ namespace WebServer
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+    }
+
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //Enable CORS
+            services.AddCors(c =>
+            {
+                c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            });
+
+
+            //JSON Serializer
+            services.AddControllersWithViews().AddNewtonsoftJson(options =>
+            options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore)
+                .AddNewtonsoftJson(options => options.SerializerSettings.ContractResolver
+                = new DefaultContractResolver());
+
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            //Enable CORS
+            app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
     }
 }
